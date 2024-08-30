9699b6b10e7070a103536e8796cf0360c0318005
Hernan Kleinubing <hernan.k97@gmail.com>
2024-08-30 09:17:56 +0200

    added price model for database

---
+++ prices/src/main/java/com/challenge/prices/domain/models/Price.java
@@ -0,0 +1,45 @@
+package com.challenge.prices.domain.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import jakarta.persistence.*;
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "prices")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Price {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "brand_id", nullable = false)
+    private Long brandId;
+
+    @Column(name = "start_date", nullable = false)
+    private LocalDateTime startDate;
+
+    @Column(name = "end_date", nullable = false)
+    private LocalDateTime endDate;
+
+    @Column(name = "price_list", nullable = false)
+    private Integer priceList;
+
+    @Column(name = "product_id", nullable = false)
+    private Long productId;
+
+    @Column(name = "priority", nullable = false)
+    private Integer priority;
+
+    @Column(name = "price", nullable = false)
+    private BigDecimal price;
+
+    @Column(name = "currency", nullable = false)
+    private String currency;
+}
