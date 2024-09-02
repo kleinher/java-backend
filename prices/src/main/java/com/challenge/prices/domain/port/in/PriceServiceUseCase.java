7f0b348306f1453ed3ec18a01a7a0893a7aa2ace
Hernan Kleinubing <hernan.k97@gmail.com>
2024-09-02 14:16:50 +0200

    Renamed Price Service class

--- prices/src/main/java/com/challenge/prices/domain/port/in/PriceServiceUseCase.java
+++ prices/src/main/java/com/challenge/prices/domain/port/in/PriceServiceUseCase.java
@@ -5,6 +5,6 @@ import com.challenge.prices.util.dto.PriceDTO;
 import java.time.LocalDateTime;
 import java.util.Optional;
 
-public interface GetPriceUseCase {
+public interface PriceServiceUseCase {
     Optional<PriceDTO> getPrice(Long productId, Long brandId, LocalDateTime applicationDate);
 }
