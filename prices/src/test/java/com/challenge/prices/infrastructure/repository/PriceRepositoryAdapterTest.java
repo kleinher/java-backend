c0d9a9bdcd8e95d4bd7d55fd9680e69c5510c305
Hernan Kleinubing <hernan.k97@gmail.com>
2024-09-05 23:22:28 +0200

    added repository addapter tests

---
+++ prices/src/test/java/com/challenge/prices/infrastructure/repository/PriceRepositoryAdapterTest.java
@@ -0,0 +1,70 @@
+package com.challenge.prices.infrastructure.repository;
+
+import com.challenge.prices.domain.models.Price;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.when;
+
+public class PriceRepositoryAdapterTest {
+
+    @Mock
+    private JPAPriceRepository jpaPriceRepository;
+
+    @InjectMocks
+    private PriceRepositoryAdapter priceRepositoryAdapter;
+
+    @BeforeEach
+    public void setup(){
+        MockitoAnnotations.openMocks(this);
+    }
+
+    @Test
+    public void test() {
+        // Given
+        long productId = 1;
+        long brandId = 35455;
+        LocalDateTime applicationDate = LocalDateTime.parse("2024-09-02T12:00:00");
+
+        Price price = Price.builder()
+                .id(1L)
+                .productId(productId)
+                .brandId(brandId)
+                .priceList(1)
+                .priority(0)
+                .startDate(LocalDateTime.parse("2024-06-14T00:00:00"))
+                .endDate(LocalDateTime.parse("2024-12-31T23:59:59"))
+                .price(BigDecimal.valueOf(35.50))
+                .currency("EUR")
+                .build();
+
+        when(jpaPriceRepository.findFirstByProductIdAndBrandIdAndStartDateLessThanEqualAndEndDateGreaterThanEqualOrderByPriorityDesc(productId,brandId,applicationDate,applicationDate)).thenReturn(Optional.of(price));
+        // When
+        Optional<Price> retrivedPrice = priceRepositoryAdapter.getPrice(productId, brandId, applicationDate);
+
+        // Then
+        assertTrue(retrivedPrice.isPresent());
+        assertEquals(retrivedPrice.get().getProductId(), productId);
+        assertEquals(retrivedPrice.get().getBrandId(), brandId);
+        assertEquals(retrivedPrice.get().getPriceList(), 1);
+        assertEquals(retrivedPrice.get().getPriority(), 0);
+        assertEquals(retrivedPrice.get().getStartDate(), LocalDateTime.parse("2024-06-14T00:00"));
+        assertEquals(retrivedPrice.get().getEndDate(), LocalDateTime.parse("2024-12-31T23:59:59"));
+        assertEquals(retrivedPrice.get().getPrice(), BigDecimal.valueOf(35.50));
+        assertEquals(retrivedPrice.get().getCurrency(), "EUR");
+
+    }
+
+
+
+
+}
