4f53da292c13183679246cc35ca88a3c8327bbeb
Hernan Kleinubing <hernan.k97@gmail.com>
2024-09-06 00:14:32 +0200

    Renamed DB table name to products

--- prices/src/main/resources/data.sql
+++ prices/src/main/resources/data.sql
@@ -1,5 +1,5 @@
 
-INSERT INTO prices (brand_id, start_date, end_date, price_list, product_id, priority, product, currency)
+INSERT INTO products (brand_id, start_date, end_date, price_list, product_id, priority, price, currency)
 VALUES
 (1, '2020-06-14 00:00:00', '2020-12-31 23:59:59', 1, 35455, 0, 35.50, 'EUR'),
 (1, '2020-06-14 15:00:00', '2020-06-14 18:30:00', 2, 35455, 1, 25.45, 'EUR'),
