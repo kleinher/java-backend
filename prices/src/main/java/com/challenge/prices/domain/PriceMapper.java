631e19a7d36759fe7a6329247907b545420761ee
Hernan Kleinubing <hernan.k97@gmail.com>
2024-09-10 12:52:02 +0200

    Fixed Price mapper to have brand entity

--- prices/src/main/java/com/challenge/prices/domain/PriceMapper.java
+++ prices/src/main/java/com/challenge/prices/domain/PriceMapper.java
@@ -1,6 +1,6 @@
 package com.challenge.prices.domain;
 
-import com.challenge.prices.domain.models.Product;
+import com.challenge.prices.domain.models.Prices;
 import com.challenge.prices.domain.models.PriceDTO;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
@@ -9,19 +9,19 @@ import org.mapstruct.Mapping;
 public interface PriceMapper {
 
     @Mapping(source = "productId", target = "productId")
-    @Mapping(source = "brandId", target = "brandId")
+    @Mapping(source = "brand.id", target = "brandId")
     @Mapping(source = "priceList", target = "priceList")
     @Mapping(source = "startDate", target = "startDate")
     @Mapping(source = "endDate", target = "endDate")
     @Mapping(source = "price", target = "price")
     @Mapping(source = "currency", target = "currency")
-    PriceDTO toDto(Product product);
+    PriceDTO toDto(Prices prices);
 
     @Mapping(source = "productId", target = "productId")
-    @Mapping(source = "brandId", target = "brandId")
+    @Mapping(source = "brandId", target = "brand.id")
     @Mapping(source = "priceList", target = "priceList")
     @Mapping(source = "startDate", target = "startDate")
     @Mapping(source = "endDate", target = "endDate")
     @Mapping(source = "price", target = "price")
-    Product toEntity(PriceDTO priceDTO);
+    Prices toEntity(PriceDTO priceDTO);
 }
