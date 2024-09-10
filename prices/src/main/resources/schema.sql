22ca383939d496f2d899e5d5363d219500dc7ba6
Hernan Kleinubing <hernan.k97@gmail.com>
2024-09-10 12:53:30 +0200

    added brand table

--- prices/src/main/resources/schema.sql
+++ prices/src/main/resources/schema.sql
@@ -1,11 +1,17 @@
-CREATE TABLE products (
+CREATE TABLE brands (
+                       id BIGINT AUTO_INCREMENT PRIMARY KEY,
+                       name VARCHAR(10) NOT NULL
+);
+
+CREATE TABLE prices (
                         id BIGINT AUTO_INCREMENT PRIMARY KEY,
-                        brand_id BIGINT NOT NULL,
+                        brand_id BIGINT,
                         start_date TIMESTAMP NOT NULL,
                         end_date TIMESTAMP NOT NULL,
                         price_list INT NOT NULL,
                         product_id BIGINT NOT NULL,
                         priority INT NOT NULL,
                         price DECIMAL(19, 2) NOT NULL,
-                        currency VARCHAR(3) NOT NULL
+                        currency VARCHAR(3) NOT NULL,
+                        CONSTRAINT fk_brand FOREIGN KEY (brand_id) REFERENCES brands(id)
 );
